9cb97add5710b13f39963aa2b3c669fa09313ca4
zhangjing <zhangjing20091105@126.com>
2023-01-05 21:53:36 +0800

    add login func and test a bit(not ok) my dad force me off

--- custlang.py
+++ custlang.py
@@ -2,7 +2,8 @@ lang_map_zh_cn={
 	"argv_miss_wrong":"参数错误或缺失",
 	"token_err":"token过期、错误或缺失",
 	"blacklisted":"已被服务器拉黑",
-	"user_login_fail":"账号/密码错误"
+	"user_login_fail":"账号/密码错误",
+	"unknown_stat":"未知返回状态"
 }
 # Select language to use
 # 选择要用的语言（yml还在鸽）
--- httpApiUTIL.py
+++ httpApiUTIL.py
@@ -1,14 +1,16 @@
 import requests,json
 from custlang import *
 APIROOTURI="public.ghs.wiki:7001"
-del(dict)
+from logging import basicConfig,getLogger
+api_log=getLogger(name="API")
+#del(dict)
 """
 Returns:
 return[0]
 	-1: http error
 	1: no error, request sucess
 	other: status code def by MossFrp
-return[1:]
+return[1]
 	if success, the returned dict
 	if error, the err msg mapping
 """
@@ -33,28 +35,38 @@ sp_map={
 	"codeaddband": {}
 }
 
-def reqAPI(string:type,dict:cargs):
+def reqAPI(rtype : str,cargs : dict[str,str]):
     global token
     api_req_root="http://"+APIROOTURI+"/API"
     newargs=cargs.copy()
-    newargs["type"]=type
-    if type!="login":
+    newargs["type"]=rtype
+    if rtype!="login":
         newargs["token"]=token
     req=requests.get(api_req_root,data=newargs)
+    #print(req.raw)
     if req.status_code!=200:
         return -1,"HTTP Protocol Error"
     retdat=json.loads(req.text)
-    resstat=retdat["status"]
-    if(resstat==200): return (1,resstat)
+    resstat=int(retdat["status"])
+    if(resstat==200): return (1,retdat)
     if resstat in errmap.keys():
         return_info=errmap[resstat]
-    elif resstat in sp_map[type].keys():
-        return_info=sp_map[type][resstat]
+    elif resstat in sp_map[rtype].keys():
+        return_info=sp_map[rtype][resstat]
+    else: return_info=langmap["unknown_stat"] #"Unknown status"
     return (resstat,return_info)
 
-def m_login(loginType,acnt,passwd):
-    data=dict()
+def m_login(loginType:str,acnt:str,passwd:str):
+    data={} #dict()
     data["loginType"]=loginType
     data["password"]=passwd
     data["account"]=acnt
-    r=reqAPI("login",data)
+    r=reqAPI("login",data) # r[0] return code; r[1] msg or data
+    if r[0]==1:
+        global token
+        token=r[1]["token"] #type:ignore # we love pylance
+        api_log.debug(str(r[0])+r[1])
+        return True
+    else:
+        api_log.warning(str(r[0])+": "+r[1])
+        return False
